c3976f3de94b4df1886a0b5bf371df615fa520a9
wheretheyeyoatdog67 <ec322@njit.edu.com>
2021-02-07 17:16:04 -0500

    hi

--- sketch.js
+++ sketch.js
@@ -294,12 +294,8 @@ class effects{
   //wood cutting
   //etc
 }
-function noScroll() {
-  window.scrollTo(0, 0);
+function disableScrolling(){
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);};
 }
-
-// add listener to disable scroll
-window.addEventListener('scroll', noScroll);
-
-// Remove listener to re-enable scroll
-window.removeEventListener('scroll', noScroll);
