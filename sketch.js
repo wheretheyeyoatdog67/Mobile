a69c47f8209e19604e2a3bb0e43154821407990f
wheretheyeyoatdog67 <ec322@njit.edu.com>
2021-02-07 17:19:15 -0500

    hi

--- sketch.js
+++ sketch.js
@@ -294,8 +294,3 @@ class effects{
   //wood cutting
   //etc
 }
-function disableScrolling(){
-    var x=window.scrollX;
-    var y=window.scrollY;
-    window.onscroll=function(){window.scrollTo(x, y);};
-}
